c8f13a7e4d5890194031f905116e3bbeb46ab583
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-05-03 20:33:32 +1200

    Draw a block

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::backend::CrosstermBackend;
+use tui::widgets::{Block, Borders};
+use tui::Terminal;
+
+fn main() -> Result<(), io::Error> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|mut frame| {
+            let size = frame.size();
+            let block = Block::default().title("Block").borders(Borders::ALL);
+            frame.render_widget(block, size);
+        })?
+    }
 }
